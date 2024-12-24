88953a761f277e69588626a49a472b1566c23954
pat2echo <pat2echo@gmail.com>
2024-12-24 11:28:07 +0100

    train sprint 1

--- code.py
+++ code.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
@@ -7,7 +10,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, f1_score
 
 class SentimentAnalyzer:
     def __init__(self):
@@ -21,16 +24,68 @@ class SentimentAnalyzer:
         self.labels = ["positive", "negative", "positive", "negative", "positive", "negative", "positive", "negative",
           "positive", "negative"]
 
-    def generative_model(self):
+    def clean_text(self, row):
+        # Preserve original text
+        original_text = row['text']
+
+        if isinstance(original_text, str):
+            # Remove URLs
+            text = re.sub(r'http\S+|www\S+|https\S+', '', original_text, flags=re.MULTILINE)
+            # Remove video tags
+            # text = re.sub(r'\[video.*?\].*?\[/video\]', '', text, flags=re.MULTILINE)
+            text = re.sub(r'(?i)\[\[video.*?\]\]', '', text)
+            # Strip HTML tags
+            text = re.sub(r'<br.*?>', '\n', text)
+            # Strip HTML tags
+            text = re.sub(r'<.*?>', ' ', text)
+
+            # Remove non-alphabetic characters
+            text = re.sub(r'[^a-zA-Z\s]', '', text)
+
+            # Remove double spaces
+            text = re.sub(r'  ', ' ', text)
+            # Convert to lower case
+            text = text.lower()
+
+            # Truncate text to the maximum number of words that the model can handle
+            tokens = text.split()
+            wc = len(tokens)
+            max_words = 200
+            if wc > max_words:
+                text = ' '.join(tokens[:max_words])
+
+            row['text'] = text
+
+        return row
+
+    def get_train_data(self, train_file, val_file):
+        train_df = pd.read_csv(train_file)
+        val_df = pd.read_csv(val_file)
+
+        # train_data = train_df.sample(frac=0.1).reset_index(drop=True)  # Shuffling and selecting 10% of data
+        #print(train_df[train_df.isna().any(axis=1)])
+        print(train_df.head())
+        train_df = train_df.dropna().apply(self.clean_text, axis=1)
+        print(f'Training data: {len(train_df)} records')
+        print(f'Validation data: {len(val_df)} records')
+
+        return train_df, val_df
+
+    def train_gen(self, train_file, val_file):
         print("*************************************")
         print("----GENERATIVE MODEL: NAIVE BAYES----")
         print("*************************************")
+
+        train_df, val_df = self.get_train_data(train_file, val_file)
+        #return
+
         # Convert text to feature vectors
+        # Note no pre-processing is done here. In practice you will have different preprocessing steps.
         vectorizer = CountVectorizer()
-        X = vectorizer.fit_transform(self.reviews)
+        X = vectorizer.fit_transform(train_df['text'])
 
         # Split into train and test sets
-        X_train, X_test, y_train, y_test = train_test_split(X, self.labels, test_size=0.2, random_state=self.registration_number)
+        X_train, X_test, y_train, y_test = train_test_split(X, train_df['sentiment'], test_size=0.2, random_state=self.registration_number)
 
         # Train Naive Bayes model
         model = MultinomialNB()
@@ -40,13 +95,14 @@ class SentimentAnalyzer:
         y_pred = model.predict(X_test)
         self.evaluation_results(y_test, y_pred)
 
-    def discriminative_model(self):
+    def train_dis(self, train_file, val_file):
         print("*********************************")
         print("----DISCRIMINATIVE MODEL: SVM----")
         print("*********************************")
+        train_df, val_df = self.get_train_data(train_file, val_file)
 
         # Split into train and test sets
-        X_train, X_test, y_train, y_test = train_test_split(self.reviews, self.labels, test_size=0.2,
+        X_train, X_test, y_train, y_test = train_test_split(train_df['text'], train_df['sentiment'], test_size=0.2,
                                                             random_state=self.registration_number)
 
         # Step 2: Feature Extraction using TF-IDF
@@ -67,9 +123,12 @@ class SentimentAnalyzer:
     def evaluation_results(self, y_test, y_pred):
         # Print the accuracy and detailed classification report
         print(f"Accuracy: {accuracy_score(y_test, y_pred):.2f}")
+        print(f"Macro F1 Score: {f1_score(y_test, y_pred, average='macro'):.2f}")
         print("Classification Report:")
         print(classification_report(y_test, y_pred, zero_division=0, output_dict=False))
 
-sa = SentimentAnalyzer()
-sa.generative_model()
-sa.discriminative_model()
+gen = SentimentAnalyzer()
+gen.train_gen(train_file='./data/train.csv', val_file='./data/valid.csv')
+
+dis = SentimentAnalyzer()
+dis.train_dis(train_file='./data/train.csv', val_file='./data/valid.csv')
